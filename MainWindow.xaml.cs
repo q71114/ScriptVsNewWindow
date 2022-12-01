671da2d3af7d2ac4c545e5a4d32fa1ab0e07469d
ibebbs <github@bebbs.co.uk>
2022-12-01 14:58:29 +0000

    Added logging

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,18 +3,9 @@ using Microsoft.Web.WebView2.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ScriptVsNewWindow
 {
@@ -25,6 +16,8 @@ namespace ScriptVsNewWindow
     {
         private readonly ObservableCollection<string> log = new();
 
+        private bool enableLog = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,12 +25,44 @@ namespace ScriptVsNewWindow
             _ = InitializeAsync();
         }
 
+        public ICollection<string> Log => this.log;
+
         private async Task InitializeAsync()
         {
             await WebView.EnsureCoreWebView2Async();
 
             WebView.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+            WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
             WebView.NavigateToString(HTML.OpenWindow);
+        }
+
+        private void LogEvent(string @event)
+        {
+            if (this.enableLog)
+            {
+                log.Add($"[{DateTime.Now.ToString("hh:mm:ss.ffff")}] {@event}");
+            }
+        }
+
+        private void CoreWebView2_NavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            if (sender is CoreWebView2 webView)
+            {
+                LogEvent($"NavigationStarting - '{webView.Source}'");
+            }
+        }
+
+        private void CoreWebView2_ContentLoading(object? sender, CoreWebView2ContentLoadingEventArgs e)
+        {
+            if (sender is CoreWebView2 webView)
+            {
+                LogEvent($"ContentLoading - '{webView.Source}'");
+            }
+        }
+
+        private void CoreWebView2_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            this.enableLog = true;
         }
 
         private async void CoreWebView2_NewWindowRequested(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2NewWindowRequestedEventArgs e)
@@ -70,14 +95,23 @@ namespace ScriptVsNewWindow
 
             await newWebView.EnsureCoreWebView2Async();
 
+            newWebView.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+            newWebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+            newWebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+            newWebView.CoreWebView2.ContentLoading += CoreWebView2_ContentLoading;
+
             if (SetScripts.SelectedIndex == 1)
             {
+                LogEvent($"Start Loading Scripts");
                 await newWebView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("alert('NewWindowRequested')");
+                LogEvent($"Completed Loading Scripts");
             }
 
             if (SetNewWindow.IsChecked == true)
             {
+                LogEvent($"Assigning NewWindow");
                 e.NewWindow = newWebView.CoreWebView2;
+                LogEvent($"Assigned NewWindow");
             }
 
             if (Delay.SelectedIndex > 0)
@@ -87,16 +121,37 @@ namespace ScriptVsNewWindow
 
             if (SetScripts.SelectedIndex == 0)
             {
+                LogEvent($"Start Loading Scripts");
                 await newWebView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("alert('NewWindowRequested')");
+                LogEvent($"Completed Loading Scripts");
             }
 
             if (SetSource.IsChecked == true)
             {
+                LogEvent($"Setting Source - '{e.Uri}'");
                 newWebView.Source = new Uri(e.Uri);
+                LogEvent($"Set Source - '{e.Uri}'");
             }
 
             e.Handled = true;
             deferral.Complete();
+        }
+
+        private void CopyLog_Click(object sender, RoutedEventArgs e)
+        {
+            var builder = new StringBuilder();
+
+            foreach (var @event in this.log)
+            {
+                builder.AppendLine(@event);
+            }
+
+            Clipboard.SetText(builder.ToString());
+        }
+
+        private void ClearLog_Click(object sender, RoutedEventArgs e)
+        {
+            this.log.Clear();
         }
     }
 }
